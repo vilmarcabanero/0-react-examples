ddcc9e6b21f97304c7f3510819b7874790941cd1
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-26 21:37:54 +0800

    Back to zero on slide transition

--- src/App.js
+++ src/App.js
@@ -1,75 +1,17 @@
-//Old
+import './App.css';
 import ParticleBackground from './Particles/ParticleBackground'
 import PracticeTests from './PracticeTests/PracticeTests'
 // import Videos from './GdriveVideos/Videos'
-import RenderHTML from './RenderHTML/RenderHTML'
 
-import React, { Component } from 'react'
-import './App.css'
-import HomePage from './pages/homePage'
-import { Switch, Route, withRouter } from 'react-router-dom'
-import AboutPage from './pages/aboutPage'
 
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
-
-import './pageTransitions/slideTransition.scss'
-
-class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			prevDepth: this.getPathDepth(this.props.location),
-		}
-	}
-
-	componentWillReceiveProps() {
-		this.setState({ prevDepth: this.getPathDepth(this.props.location) })
-	}
-
-	getPathDepth(location) {
-		let pathArr = location.pathname.split('/')
-		pathArr = pathArr.filter(n => n !== '')
-		return pathArr.length
-	}
-
-	render() {
-		const { location } = this.props
-
-		const currentKey = location.pathname.split('/')[1] || '/'
-		const timeout = { enter: 800, exit: 400 }
-
-		return (
-			<>
-				<TransitionGroup component='div' className='App'>
-					<CSSTransition
-						key={currentKey}
-						timeout={timeout}
-						classNames='pageSlider'
-						mountOnEnter={false}
-						unmountOnExit={true}
-					>
-						<div
-							className={
-								this.getPathDepth(location) - this.state.prevDepth >= 0
-									? 'left'
-									: 'right'
-							}
-						>
-							<Switch location={location}>
-								<Route path='/' exact component={HomePage} />
-								<Route path='/about' exact component={AboutPage} />
-							</Switch>
-						</div>
-					</CSSTransition>
-				</TransitionGroup>
-
-				<ParticleBackground />
-				{/* <Videos/> */}
-				{/* <PracticeTests /> */}
-				<RenderHTML />
-			</>
-		)
-	}
+function App() {
+  return (
+    <>
+      <ParticleBackground />
+      {/* <Videos/> */}
+      <PracticeTests />
+    </>
+  );
 }
 
-export default withRouter(App)
+export default App;
