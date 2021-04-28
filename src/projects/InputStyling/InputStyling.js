030e42518ab8ed0a0f2377dc83cc9718f3bace83
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-28 23:47:10 +0800

    Finalized the design of sign up page, using pure css. Not material ui

--- src/projects/InputStyling/InputStyling.js
+++ src/projects/InputStyling/InputStyling.js
@@ -1,47 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
-import { InputBase } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-
 const gainsboro = '#DCDCDC'
 const darkGray = '#A9A9A9'
 const gray = '808080'
 const lightGray = '#D3D3D3'
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		'& > *': {
-			// margin: theme.spacing(1),
-			// width: '45ch',
-			// width: '100%'
-		},
-	},
-	email: {
-		fontSize: '1rem',
-		width: 'inherit',
-		borderRadius: theme.shape.borderRadius,
-		color: 'white',
-		borderBottom: `1px solid ${gainsboro}`,
-		height: '2.5rem',
-		padding: theme.spacing(1),
-		paddingBottom: '0px',
-		marginBottom: '1rem',
-		backgroundColor: 'transparent',
-	},
-}))
-
 const InputStyling = () => {
-	const classes = useStyles()
 	return (
 		<Component>
 			<label class='custom-input'>
-				<InputBase id='input' className={classes.email} />
+				<input required />
 				<span className='placeholder'>Email address</span>
 			</label>
-			<label class='custom-field'>
-				<input className={classes.email} required />
+			<label class='custom-input'>
+				<input required />
 				<span className='placeholder'>Email address</span>
 			</label>
+			<button>Sign in</button>
 		</Component>
 	)
 }
@@ -53,8 +28,8 @@ const Component = styled.div`
 	/* width: 28rem; //sing in page */
 	/* height: 40rem; */
 	width: 25rem;
-	padding-left: 4rem;
-	padding-right: 4rem;
+	padding-left: 3.75rem;
+	padding-right: 4.25rem;
 	padding-top: 3rem;
 	padding-bottom: 5rem;
 	border-radius: 5px;
@@ -67,36 +42,40 @@ const Component = styled.div`
 	background-color: rgba(0, 0, 0, 0.6);
 	color: white;
 
+	@media (max-width: 46rem) {
+		& {
+			width: 22.5rem;
+			position: relative;
+			margin-top: 3rem;
+			margin-bottom: 1.5rem;
+			margin-left: auto;
+			margin-right: auto;
+		}
+	}
+
 	.custom-input {
 		position: relative;
 		width: 100%;
-
-    #input {
-
-    }
-		.placeholder {
-			font-size: 1rem;
-			position: absolute;
-			left: 0.5rem;
-			top: 1rem;
-			color: ${darkGray};
-		}
-
-	}
-
-	.custom-field {
-		position: relative;
-		width: 100%;
+		margin-bottom: 1.25rem;
 
 		input {
+			font-size: 1rem;
+			width: inherit;
+			border-radius: 5px;
+			color: white;
+			border-bottom: 1px solid ${gainsboro};
+			height: 2rem;
+			padding-left: 0.5rem;
+			background-color: transparent;
 			outline: none;
 			border-top: none;
 			border-left: none;
 			border-right: none;
 
-			&:valid + .placeholder, &:focus + .placeholder {
-				top: 0;
-				font-size: 0.7rem;
+			&:valid + .placeholder,
+			&:focus + .placeholder {
+				top: -10px;
+				font-size: 0.75rem;
 				color: ${lightGray};
 			}
 		}
@@ -104,10 +83,33 @@ const Component = styled.div`
 			font-size: 1rem;
 			position: absolute;
 			left: 0.5rem;
-			top: 1.6rem;
+			top: 0.75rem;
 			color: #aaa;
 			transition: top 0.3s ease, font-size 0.3s ease, color 0.3s ease;
-      cursor: text;
+			cursor: text;
+		}
+	}
+
+	button {
+		height: 2.75rem;
+		width: 25.5rem;
+		color: white;
+		margin-right: auto;
+		margin-left: auto;
+		font-size: 1.2rem;
+		background-color: #06f;
+		border: 1px solid #06f;
+		border-radius: 0.2rem;
+		&:hover {
+			font-size: 1.2rem;
+			cursor: pointer;
+			background-color: transparent;
+		}
+
+		@media (max-width: 46rem) {
+			& {
+				width: 23rem;
+			}
 		}
 	}
 `
--- src/projects/SignUp/SignUp.js
+++ src/projects/SignUp/SignUp.js
@@ -9,91 +9,33 @@ const gray = '808080'
 const lightGray = '#D3D3D3'
 const gainsboro = '#DCDCDC'
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		'& > *': {
-			// margin: theme.spacing(1),
-			// width: '25ch',
-			// width: '100%'
-			position: 'relative',
-		},
-	},
-	password: {
-		width: '100%',
-		borderRadius: theme.shape.borderRadius,
-		color: 'white',
-		backgroundColor: 'transparent',
-		borderBottom: `1px solid ${gainsboro}`,
-		height: '2.5rem',
-		padding: theme.spacing(1),
-		paddingBottom: '0px',
-		marginBottom: '3rem',
-		marginRight: '0px',
-	},
-	name: {
-		width: '50%',
-		borderRadius: theme.shape.borderRadius,
-		color: 'white',
-		borderBottom: `1px solid ${gainsboro}`,
-		height: '2.5rem',
-		padding: theme.spacing(1),
-		paddingBottom: '0px',
-		marginBottom: '1rem',
-	},
-	button: {
-		height: '2.75rem',
-		width: '100%',
-		color: 'white',
-		fontSize: '1.2rem',
-		backgroundColor: '#06f',
-		border: '1px solid #06f',
-		// height: '45px',
-		'&:hover': {
-			fontSize: '1.2rem',
-		},
-		passwordField: {
-			marginBottom: '1rem',
-		},
-	},
-}))
-
 export default function SignUp() {
-	const classes = useStyles()
 	const [PasswordInputType, ToggleIcon] = usePasswordToggle()
 
 	return (
 		<>
 			<Styled>
 				<h1>Create an account</h1>
-				<form className={classes.root} noValidate autoComplete='off'>
-					{/* <TextField
-						id='standard-basic'
-						label='Email address'
-						variant='standard'
-						className={classes.textField}
-					/>{' '} */}
-					<InputBase
-						placeholder='First Name'
-						variant='outlined'
-						className={classes.name}
-					/>
-					<InputBase
-						placeholder='Last Name'
-						variant='outlined'
-						className={classes.name}
-					/>
+					<div className='flex'>
+						<label class='custom-input'>
+							<input className='firstname' required />
+							<span className='placeholder'>First name</span>
+						</label>
+						<label class='custom-input'>
+							<input className='lastname' required />
+							<span className='placeholder'>Last name</span>
+						</label>
+					</div>
+
 					<div className='password-container'>
-						<InputBase
-							placeholder='Password'
-							type={PasswordInputType}
-							variant='outlined'
-							className={classes.password}
-						/>
+						<label class='custom-input'>
+							<input className='password' type={PasswordInputType} required />
+							<span className='placeholder'>Password</span>
+						</label>
 						<div className='visibility'>{ToggleIcon}</div>
 					</div>
-				</form>
 
-				<Button className={classes.button}>Sign up</Button>
+					<button>Sign up</button>
 				<div>
 					<div>
 						<span>Already have an account? </span> <a href='/'> Sign in</a>
@@ -138,19 +80,116 @@ const Styled = styled.div`
 
 	h1 {
 		font-size: 2rem;
+		margin-bottom: 2rem;
+	}
+	.custom-input {
+		position: relative;
+		width: 100%;
+		margin-bottom: 1.25rem;
+
+		.firstname {
+			border-radius: 0 0 0 5px;
+		}
+
+		.lastname {
+			border-radius: 0 0 5px 0;
+		}
+
+		.password {
+			border-radius: 5px;
+			margin-bottom: 1rem;
+		}
+
+		input {
+			font-size: 1rem;
+			width: 100%;
+			/* border-radius: 5px; */
+			color: white;
+			border-bottom: 1px solid ${gainsboro};
+			height: 2rem;
+			padding-left: 0.5rem;
+			background-color: transparent;
+			outline: none;
+			border-top: none;
+			border-left: none;
+			border-right: none;
+
+			&:valid + .placeholder,
+			&:focus + .placeholder {
+				top: -10px;
+				font-size: 0.75rem;
+				color: ${lightGray};
+			}
+		}
+
+		.placeholder {
+			font-size: 1rem;
+			position: absolute;
+			left: 0.5rem;
+			top: 0.75rem;
+			color: #aaa;
+			transition: top 0.3s ease, font-size 0.3s ease, color 0.3s ease;
+			cursor: text;
+		}
+	}
+
+	.flex {
+		display: flex;
+		position: relative;
+		width: 24.5rem;
+		@media (max-width: 46rem) {
+			& {
+				width: 22rem;
+			}
+		}
 	}
 
 	.password-container {
+		position: relative;
 		display: flex;
 		align-items: center;
+		width: inherit;
+
+		
+		width: 24.5rem;
+		@media (max-width: 46rem) {
+			& {
+				width: 22rem;
+			}
+		}
 
 		.visibility {
-			margin-bottom: 2.25rem;
-			padding: 0.5rem;
+			margin-bottom: 2.5rem;
+			padding: 0.5rem 0 0.5rem 0;
 			position: absolute;
 			right: 0rem;
 			cursor: pointer;
 			z-index: 20;
+		}
+
+		
+	}
+
+	button {
+		height: 2.75rem;
+		width: 25rem;
+		color: white;
+		margin-right: auto;
+		margin-left: auto;
+		font-size: 1.2rem;
+		background-color: #06f;
+		border: 1px solid #06f;
+		border-radius: 0.2rem;
+		&:hover {
+			font-size: 1.2rem;
+			cursor: pointer;
+			background-color: transparent;
+		}
+
+		@media (max-width: 46rem) {
+			& {
+				width: 22.5rem;
+			}
 		}
 	}
 
